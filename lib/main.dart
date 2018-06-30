af9684d142154d0e86790a22daa6e710914bd8f3
Prishan Fernando <prishanf@yahoo.com>
2018-06-30 14:37:31 -0700

    Move card to  a column layout and add a button with margin

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,23 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("My First App"),
         ),
-        body: Card(
-          child: Column(
-            children: <Widget>[
-              Image.asset('assets/food.jpg'),
-              Text('Food Paradise')
-            ],
+        body: Column(children: <Widget>[
+          Container(
+            margin: EdgeInsets.all(10.0),
+            child: RaisedButton(
+              onPressed: () {},
+              child: Text('Add Product'),
+            ),
           ),
-        ),
+          Card(
+            child: Column(
+              children: <Widget>[
+                Image.asset('assets/food.jpg'),
+                Text('Food Paradise')
+              ],
+            ),
+          ),
+        ]),
       ),
     );
   }
