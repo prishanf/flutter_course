54747206524c27ea46cb21352c59c90fd0da0cf6
Prishan Fernando <prishanf@yahoo.com>
2018-06-30 15:20:37 -0700

    31 Create the product manager widget and update the main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'product_manager.dart';
+
 main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  List<String> _products = ['Food Tester'];
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("My First App1"),
+          title: Text("My First App"),
         ),
-        body: Column(children: <Widget>[
-          Container(
-            margin: EdgeInsets.all(10.0),
-            child: RaisedButton(
-              onPressed: () {
-                setState(() {
-                  _products.add('Advanced food');                  
-                });
-              },
-              child: Text('Add Product'),
-            ),
-          ),
-          
-        ]),
+        body: ProductManager(),
       ),
     );
   }
