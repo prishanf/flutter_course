462eed512b6639799a4a6f807194d1c4385a5f74
Prishan Fernando <prishanf@yahoo.com>
2018-06-30 14:43:26 -0700

    change MyApp from stateless to satefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,14 @@ main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
