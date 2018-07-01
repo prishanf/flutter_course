1ab811d9fef745974ac2d6f1df68b18ef83f4825
Prishan Fernando <prishanf@yahoo.com>
2018-06-30 18:22:33 -0700

    60 Render content Condiitionally

--- lib/products.dart
+++ lib/products.dart
@@ -18,10 +18,9 @@ class Products extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return ListView.builder(
+    return products.length > 0 ? ListView.builder(
       itemBuilder: _buildProductItem,
       itemCount: products.length,
-    );
+    ): Center(child: Text('No Products, Please add some'),);
   }
 }
