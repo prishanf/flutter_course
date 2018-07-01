b26e95fb0724ccb2646633ec7554a19773e84e46
Prishan Fernando <prishanf@yahoo.com>
2018-06-30 18:30:25 -0700

    Alternative if check to render content condiitonaly

--- lib/products.dart
+++ lib/products.dart
@@ -18,9 +18,15 @@ class Products extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return products.length > 0 ? ListView.builder(
-      itemBuilder: _buildProductItem,
-      itemCount: products.length,
-    ): Center(child: Text('No Products, Please add some'),);
+    Widget productCard = Center(
+      child: Text('No Products, Please add some'),
+    );
+    if (products.length > 0) {
+      productCard = ListView.builder(
+        itemBuilder: _buildProductItem,
+        itemCount: products.length,
+      );
+    }
+    return productCard;
   }
 }
