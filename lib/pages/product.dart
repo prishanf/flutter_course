0bce156e5e7258e7969c72cf39c8665a68a77edd
Prishan Fernando <prishanf@yahoo.com>
2018-07-01 09:57:27 -0700

    68 Imporved the look of the product page

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -9,12 +9,21 @@ class ProductPage extends StatelessWidget {
       ),
       body: Center(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Text("On the Product Page"),
-            RaisedButton(
-              child: Text('Back'),
-              onPressed: () => Navigator.pop(context),
-            )
+            Image.asset('assets/food.jpg'),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text("On the Product Page"),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                color: Theme.of(context).accentColor,
+                child: Text('Back'),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ),
           ],
         ),
       ),
