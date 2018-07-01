1542012e35451b2cd80f6caa543691c03be37276
Prishan Fernando <prishanf@yahoo.com>
2018-07-01 14:03:21 -0700

    71 Back button hadling

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,35 +1,43 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class ProductPage extends StatelessWidget {
-final String title;
-final String imageUrl;
+  final String title;
+  final String imageUrl;
 
-ProductPage({this.title,this.imageUrl});
+  ProductPage({this.title, this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(imageUrl),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              child: Text("Product Details"),
-            ),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                color: Theme.of(context).accentColor,
-                child: Text('Delete'),
-                onPressed: () => Navigator.pop(context,true),
+    return WillPopScope(
+      onWillPop: () {
+        print('Back Button Pressed');
+        Navigator.pop(context, false);
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(imageUrl),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: Text("Product Details"),
               ),
-            ),
-          ],
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  color: Theme.of(context).accentColor,
+                  child: Text('Delete'),
+                  onPressed: () => Navigator.pop(context, true),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
