3963d8c28211e82c0995b15b6e23c065373673fc
Prishan Fernando <prishanf@yahoo.com>
2018-07-02 19:47:56 -0700

    add Swtich to the auth form

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -10,6 +10,7 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   String _emailValue;
   String _passwordValue;
+  bool _acceptTerms = false;
 
   @override
   Widget build(BuildContext context) {
@@ -46,6 +47,15 @@ class _AuthPageState extends State<AuthPage> {
                   _passwordValue = value;
                 });
               },
+            ),
+            SwitchListTile(
+              value: _acceptTerms,
+              onChanged: (bool value) {
+                setState(() {
+                  _acceptTerms = value;
+                });
+              },
+              title: Text('Accept Terms'),
             ),
             SizedBox(
               height: 15.0,
