26520d33fa3a6a65beb518f1836d1009a790c342
Prishan Fernando <prishanf@yahoo.com>
2018-07-09 22:16:53 -0700

    153-155 Favorite feature

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 import '../widgets/products/products.dart';
+import '../scoped-models/products.dart';
 
 class ProductsPage extends StatelessWidget {
-
   ProductsPage();
 
   Widget _buildSideDrawer(BuildContext context) {
@@ -33,10 +34,15 @@ class ProductsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('EasyList'),
         actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.favorite),
-            onPressed: () {},
-          )
+          ScopedModelDescendant<ProductsModel>(builder:
+              (BuildContext context, Widget child, ProductsModel model) {
+            return IconButton(
+              icon: model.displayFavoritesOnly? Icon(Icons.favorite): Icon(Icons.favorite_border),
+              onPressed: () {
+                model.toggleDisplayMode();
+              },
+            );
+          }),
         ],
       ),
       body: Products(),
--- lib/scoped-models/products.dart
+++ lib/scoped-models/products.dart
@@ -6,12 +6,25 @@ class ProductsModel extends Model {
   List<Product> _products = [];
   int _selectedProductIndex;
 
+  bool _showFavorites = false;
+
   List<Product> get products {
+    return List.from(_products);
+  }
+
+  List<Product> get displayedProducts {
+    if (_showFavorites) {
+      return _products.where((Product product) => product.isFavorite).toList();
+    }
     return List.from(_products);
   }
 
   int get selectedProductIndex {
     return _selectedProductIndex;
+  }
+
+  bool get displayFavoritesOnly {
+    return _showFavorites;
   }
 
   Product get selectedProduct {
@@ -36,7 +49,31 @@ class ProductsModel extends Model {
     _selectedProductIndex = null;
   }
 
+  void toggleProductFavoriteStatus() {
+    print(toggleProductFavoriteStatus);
+    print(_products[_selectedProductIndex].isFavorite);
+    final bool isCurrentlyFavorite =
+        _products[_selectedProductIndex].isFavorite;
+    final bool newFavouriteStatus = !isCurrentlyFavorite;
+    final Product updateProduct = Product(
+        title: selectedProduct.title,
+        description: selectedProduct.description,
+        price: selectedProduct.price,
+        image: selectedProduct.image,
+        isFavorite: newFavouriteStatus);
+    _products[_selectedProductIndex] = updateProduct;
+    print(_products[_selectedProductIndex].isFavorite);
+    _selectedProductIndex = null;
+
+    notifyListeners();
+  }
+
   void selectProduct(int index) {
     _selectedProductIndex = index;
+  }
+
+  void toggleDisplayMode() {
+    _showFavorites = !_showFavorites;
+    notifyListeners();
   }
 }
